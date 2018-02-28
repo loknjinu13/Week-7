4ae1ef46b4c3d53cc07969183007b8e4a26b0a50
grenack <grenack@gmail.com>
2018-02-27 20:35:56 -0600

    My Assignment

---
+++ Week7.sh
@@ -0,0 +1,36 @@
+#Glanel Houenouvi
+#Linux
+#02-25-18
+
+
+"An Industry Guide to Becoming a software Engineer" by Bill Langerberg.
+
+During my reading, the Guide  made me  understand  what to focus on, the 
+skills needed to be  know and how data structures works and how to solve it by
+debugging it.
+I definetely need to know the essentials troubleshooting skills.
+Bill's also back his arguments with examples amd many others steps. For me 
+he has such a great dicdatic startegies of teaching people what they need 
+to know to kick-off they Software Engineering Career.How to write code and how
+to reviews code are important value of a Software Engineer.
+
+According to me, i always want to know  and understand how  language are put
+together and during execution they can explain perfect thought of a developer
+ or become an application.
+From my knowledge becoming a developer or Software engineer should be  someone
+who love language and can be able to interpret those languange also
+being able to solve any computer issue, software like hardware.You need to
+know the best languange and right tool to commentyour peers and your own work.
+
+I conclusion this is a great document that i could recommend to anyone. It has
+the correct points and words. expecially what needed to be successful in the 
+career. The best way to learn  is to keep practicing this tools and languages
+and reviewing friends work on Github, stackoverflow.
+It is all need to be a good candidate at any job interview and job search.
+
+
+Thanks    
+
+
+
+
